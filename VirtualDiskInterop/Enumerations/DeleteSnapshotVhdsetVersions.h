d9301c75d60d949cb188d19494770c7d21f7901b
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 16:28:15 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/DeleteSnapshotVhdsetVersions.h
+++ VirtualDiskInterop/Enumerations/DeleteSnapshotVhdsetVersions.h
@@ -5,9 +5,19 @@ using namespace System;
 namespace VirtualDiskInterop
 {
 #ifdef WIN10SUPPORT
+	/// <summary>
+	/// Contains the version of the DELETE_SNAPHSOT_VHDSET_PARAMETERS structure to use in calls to virtual disk functions.
+	/// </summary>
 	public enum class DeleteSnapshotVhdsetVersions
 	{
+		/// <summary>
+		/// Not supported.
+		/// </summary>
 		Unspecified = DELETE_SNAPSHOT_VHDSET_VERSION_UNSPECIFIED,
+		
+		/// <summary>
+		/// The Version1 member structure will be used.
+		/// </summary>
 		Version1 = DELETE_SNAPSHOT_VHDSET_VERSION_1
 	};
 #endif
