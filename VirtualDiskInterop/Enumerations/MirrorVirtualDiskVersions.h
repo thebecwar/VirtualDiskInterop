1037dc03fbc9d93b038ee35f874823f2431d52de
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 19:30:50 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/MirrorVirtualDiskVersions.h
+++ VirtualDiskInterop/Enumerations/MirrorVirtualDiskVersions.h
@@ -4,9 +4,19 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains the version of the virtual disk MIRROR_VIRTUAL_DISK_PARAMETERS structure used by the MirrorVirtualDisk function.
+	/// </summary>
 	public enum class MirrorVirtualDiskVersions
 	{
+		/// <summary>
+		/// Unsupported
+		/// </summary>
 		Unspecified = MIRROR_VIRTUAL_DISK_VERSION_UNSPECIFIED,
+		
+		/// <summary>
+		/// Use the Version1 member.
+		/// </summary>
 		Version1 = MIRROR_VIRTUAL_DISK_VERSION_1,
 	};
 }
