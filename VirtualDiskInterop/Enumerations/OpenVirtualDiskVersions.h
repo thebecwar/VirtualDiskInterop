73956f09c48955cd58306edf5c854ac070b83190
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 21:56:54 +0200

    Added comment blocks and missing member

--- VirtualDiskInterop/Enumerations/OpenVirtualDiskVersions.h
+++ VirtualDiskInterop/Enumerations/OpenVirtualDiskVersions.h
@@ -4,10 +4,14 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains the version of the virtual hard disk (VHD) OPEN_VIRTUAL_DISK_PARAMETERS structure to use in calls to VHD functions.
+	/// </summary>
 	public enum class OpenVirtualDiskVersions
 	{
 		Unspecified = OPEN_VIRTUAL_DISK_VERSION_UNSPECIFIED,
 		Version1 = OPEN_VIRTUAL_DISK_VERSION_1,
 		Version2 = OPEN_VIRTUAL_DISK_VERSION_2,
+		Version3 = OPEN_VIRTUAL_DISK_VERSION_3
 	};
 }
