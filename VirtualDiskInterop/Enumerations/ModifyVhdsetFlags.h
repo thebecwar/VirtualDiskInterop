bc4485b6e34d3a9970f66cb20945bdc2003ad1e7
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 21:49:34 +0200

    Added comment blocks and missing member

--- VirtualDiskInterop/Enumerations/ModifyVhdsetFlags.h
+++ VirtualDiskInterop/Enumerations/ModifyVhdsetFlags.h
@@ -5,10 +5,18 @@ using namespace System;
 namespace VirtualDiskInterop
 {
 #ifdef WIN10SUPPORT
+	/// <summary>
+	/// Contains flags affecting the behavior of the ModifyVhdSet function.
+	/// </summary>
 	[Flags()]
 	public enum class ModifyVhdsetFlags
 	{
+		/// <summary>
+		/// No flag specified.
+		/// </summary>
 		None = MODIFY_VHDSET_FLAG_NONE,
+		
+		WriteableSnapshot = MODIFY_VHDSET_FLAG_WRITEABLE_SNAPSHOT
 	};
 #endif
 }
