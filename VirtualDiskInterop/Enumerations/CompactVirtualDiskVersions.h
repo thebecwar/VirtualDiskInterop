9892cfe87fecafd03dff45582907792f9224073c
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 16:17:34 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/CompactVirtualDiskVersions.h
+++ VirtualDiskInterop/Enumerations/CompactVirtualDiskVersions.h
@@ -4,6 +4,9 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains the version of the virtual hard disk (VHD) COMPACT_VIRTUAL_DISK_PARAMETERS structure to use in calls to VHD functions.
+	/// </summary>
 	public enum class CompactVirtualDiskVersions
 	{
 		Unspecified = COMPACT_VIRTUAL_DISK_VERSION_UNSPECIFIED,
