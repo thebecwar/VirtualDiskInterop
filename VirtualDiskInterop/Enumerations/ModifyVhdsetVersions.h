61ce0206d8e4449c95f40579450ebfbce8c5991a
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 21:52:48 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/ModifyVhdsetVersions.h
+++ VirtualDiskInterop/Enumerations/ModifyVhdsetVersions.h
@@ -5,11 +5,29 @@ using namespace System;
 namespace VirtualDiskInterop
 {
 #ifdef WIN10SUPPORT
+	/// <summary>
+	/// Contains the version of the MODIFY_VHDSET_PARAMETERS structure to use in calls to virtual disk functions.
+	/// </summary>
 	public enum class ModifyVhdsetVersions
 	{
+		/// <summary>
+		/// Not Supported.
+		/// </summary>
 		Unspecified = MODIFY_VHDSET_UNSPECIFIED,
+		
+		/// <summary>
+		/// The SnapshotPath member structure will be used.
+		/// </summary>
 		SnapshotPath = MODIFY_VHDSET_SNAPSHOT_PATH,
+		
+		/// <summary>
+		/// The SnapshotId member structure will be used.
+		/// </summary>
 		RemoveSnapshot = MODIFY_VHDSET_REMOVE_SNAPSHOT,
+		
+		/// <summary>
+		/// The DefaultFilePath member structure will be used.
+		/// </summary>
 		DefaultSnapshotPath = MODIFY_VHDSET_DEFAULT_SNAPSHOT_PATH,
 	};
 #endif
