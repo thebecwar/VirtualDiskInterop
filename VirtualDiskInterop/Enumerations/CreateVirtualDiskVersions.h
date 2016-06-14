821989e94d39ebb9e1bbf25be3feb5a31231c8e5
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 16:24:12 +0200

    Added comment blocks and missing member

--- VirtualDiskInterop/Enumerations/CreateVirtualDiskVersions.h
+++ VirtualDiskInterop/Enumerations/CreateVirtualDiskVersions.h
@@ -4,10 +4,14 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains the version of the virtual hard disk (VHD) CreateVirtualDiskParameters structure to use in calls to VHD functions.
+	/// </summary>
 	public enum class CreateVirtualDiskVersions
 	{
 		Unspecified = CREATE_VIRTUAL_DISK_VERSION_UNSPECIFIED,
 		Version1 = CREATE_VIRTUAL_DISK_VERSION_1,
 		Version2 = CREATE_VIRTUAL_DISK_VERSION_2,
+		Version3 = CREATE_VIRTUAL_DISK_VERSION_3
 	};
 }
