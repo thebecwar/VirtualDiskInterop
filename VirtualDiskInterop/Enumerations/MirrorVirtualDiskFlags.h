100b5e9c2be7fc9fdf24aae5f6c9c2614f8e47d8
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 18:36:54 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/MirrorVirtualDiskFlags.h
+++ VirtualDiskInterop/Enumerations/MirrorVirtualDiskFlags.h
@@ -4,10 +4,20 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains virtual hard disk (VHD) mirror request flags.
+	/// </summary> 
 	[Flags()]
 	public enum class MirrorVirtualDiskFlags
 	{
+		/// <summary>
+		/// The mirror virtual disk file does not exist, and needs to be created.
+		/// </summary>
 		None = MIRROR_VIRTUAL_DISK_FLAG_NONE,
+		
+		/// <summary>
+		/// Create the mirror using an existing file.
+		/// </summary>
 		ExistingFile = MIRROR_VIRTUAL_DISK_FLAG_EXISTING_FILE,
 	};
 }
