2ac4b746d17f02a4e4d237098654218c34e913f0
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 16:16:39 +0200

    Added comment blocks and missing members

--- VirtualDiskInterop/Enumerations/CompactVirtualDiskFlags.h
+++ VirtualDiskInterop/Enumerations/CompactVirtualDiskFlags.h
@@ -4,9 +4,14 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains virtual hard disk (VHD) compact request flags.
+	/// </summary>
 	[Flags()]
 	public enum class CompactVirtualDiskFlags
 	{
 		None = COMPACT_VIRTUAL_DISK_FLAG_NONE,
+		NoZeroScan = COMPACT_VIRTUAL_DISK_FLAG_NO_ZERO_SCAN,
+		NoBlockMoves = COMPACT_VIRTUAL_DISK_FLAG_NO_BLOCK_MOVES
 	};
 }
