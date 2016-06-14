a6cd4614d8b81e3b5752fce1f26f6594bb578ed6
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 22:03:20 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/ResizeVirtualDiskVersions.h
+++ VirtualDiskInterop/Enumerations/ResizeVirtualDiskVersions.h
@@ -3,9 +3,19 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Enumerates the possible versions for parameters for the ResizeVirtualDisk function.
+	/// </summary>
 	public enum class ResizeVirtualDiskVersions
 	{
+		/// <summary>
+		/// The version is not valid.
+		/// </summary>
 		Unspecified = RESIZE_VIRTUAL_DISK_VERSION_UNSPECIFIED,
+		
+		/// <summary>
+		/// Version one of the parameters is used. This is the only supported value.
+		/// </summary>
 		Version1 = RESIZE_VIRTUAL_DISK_VERSION_1,
 	};
 }
