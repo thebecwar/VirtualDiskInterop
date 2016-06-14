87afb41bf9b8576d209eabb0a5cc85b712475abf
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 22:07:03 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/StorageDependencyInfoVersions.h
+++ VirtualDiskInterop/Enumerations/StorageDependencyInfoVersions.h
@@ -4,8 +4,14 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains the version of the virtual hard disk (VHD) STORAGE_DEPENDENCY_INFO structure to use in calls to VHD functions.
+	/// </summary>
 	public enum class StorageDependencyInfoVersions
 	{
+		/// <summary>
+		/// The version is not specified.
+		/// </summary>
 		Unspecified = STORAGE_DEPENDENCY_INFO_VERSION_UNSPECIFIED,
 		Version1 = STORAGE_DEPENDENCY_INFO_VERSION_1,
 		Version2 = STORAGE_DEPENDENCY_INFO_VERSION_2,
