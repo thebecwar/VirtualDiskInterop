244068d9846f325755082888a5dccc54b79b5c05
Mark Becwar <mark@thebecwar.com>
2016-06-14 16:34:24 -0400

    Add QueryChangesVirtualDiskFlags.h list

--- VirtualDiskInterop/VirtualDiskInterop.cpp
+++ VirtualDiskInterop/VirtualDiskInterop.cpp
@@ -30,6 +30,7 @@
 #include "Enumerations\ModifyVhdsetVersions.h"
 #include "Enumerations\OpenVirtualDiskFlags.h"
 #include "Enumerations\OpenVirtualDiskVersions.h"
+#include "Enumerations\QueryChangesVirtualDiskFlags.h"
 #include "Enumerations\ResizeVirtualDiskFlags.h"
 #include "Enumerations\ResizeVirtualDiskVersions.h"
 #include "Enumerations\SetVirtualDiskInfoVersions.h"
