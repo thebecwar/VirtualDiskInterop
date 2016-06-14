5a7c3481e4e5a94974a064b34c83228cfe7e7829
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 16:46:04 +0200

    Added comment blocks

--- VirtualDiskInterop/Enumerations/GetStorageDependencyFlags.h
+++ VirtualDiskInterop/Enumerations/GetStorageDependencyFlags.h
@@ -4,11 +4,25 @@ using namespace System;
 
 namespace VirtualDiskInterop
 {
+	/// <summary>
+	/// Contains virtual hard disk (VHD) storage dependency request flags.
+	/// </summary>
 	[Flags()]
 	public enum class GetStorageDependencyFlags
 	{
+		/// <summary>
+		/// No flags specified.
+		/// </summary>
 		None = GET_STORAGE_DEPENDENCY_FLAG_NONE,
+		
+		/// <summary>
+		/// Return information for volumes or disks hosting the volume specified. 
+		/// </summary>
 		HostVolumes = GET_STORAGE_DEPENDENCY_FLAG_HOST_VOLUMES,
+		
+		/// <summary>
+		/// The handle provided is to a disk, not a volume or file.
+		/// </summary>
 		DiskHandle = GET_STORAGE_DEPENDENCY_FLAG_DISK_HANDLE,
 	};
 }
