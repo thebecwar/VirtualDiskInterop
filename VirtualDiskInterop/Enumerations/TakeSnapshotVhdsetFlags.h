671399cef1f264c691ba2ff3bbc687dda8b16b37
FaustoNascimento <faustoconsultancy@outlook.com>
2016-06-14 22:08:07 +0200

    Added comment blocks and missing member

--- VirtualDiskInterop/Enumerations/TakeSnapshotVhdsetFlags.h
+++ VirtualDiskInterop/Enumerations/TakeSnapshotVhdsetFlags.h
@@ -5,10 +5,14 @@ using namespace System;
 namespace VirtualDiskInterop
 {
 #ifdef WIN10SUPPORT
+	/// <summary>
+	/// Contains flags affecting the behavior of the TakeSnapshotVhdSet function.
+	/// </summary>
 	[Flags()]
 	public enum class TakeSnapshotVhdsetFlags
 	{
 		None = TAKE_SNAPSHOT_VHDSET_FLAG_NONE,
+		Writeable = TAKE_SNAPSHOT_VHDSET_FLAG_WRITEABLE
 	};
 #endif
 }
